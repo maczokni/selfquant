5f4ce7862d987a6eb64e75e17a119c3038a4e845
maczokni <maczokni@gmail.com>
2017-05-26 19:44:20 +0100

    getQuant function added

---
+++ R/getQuant.R
@@ -0,0 +1,18 @@
+##' getQuant function reads in your weekly stats from your google steet
+#'
+#' This function reads your monthly stats from a google sheet to a dataframe
+#' @param title parameter for the name of the google sheet, defaults to Self Quant
+#' @param workSheet parameter for the name of the worksheet within the doc, defaults to Template
+#' @keywords quantified self
+#' @export
+#' @examples
+#' some example here
+
+getQuant <- function(title="Self Quant", workSheet="Template") {(my_sheets <- gs_ls())
+  selfQuant <- gs_title(title)
+  sq_data <- selfQuant %>%
+    gs_read(ws = workSheet)
+  sq_data <- head(sq_data, n=9)
+  return(sq_data)
+}
+
