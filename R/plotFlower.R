f59f020b840e9c03196329bad12b1b6c90d51e06
maczokni <maczokni@gmail.com>
2017-05-27 01:19:31 +0100

    added comments for clarity

--- R/plotFlower.R
+++ R/plotFlower.R
@@ -14,13 +14,21 @@
 
 plotFlower <- function(sq_summary){
 
+  #first subset the dataframe to contain only the metric variable (x1) and the net scores for each week
   netCols <- dplyr::select(sq_summary, X1, gtb_w1, gtb_w2, gtb_w3, gtb_w4)
+
+  #then gather to get week as one variable
   netColsSum <- tidyr::gather(netCols, "week", "gtb", 2:5)
+
+  #rename the metric column
   names(netColsSum)[1]<-"Metric"
+
+  #get the mean good to bad ratio over all 4 weeks for each metric
   petalDf <- netColsSum %>%
               dplyr::group_by(Metric) %>%
               dplyr::summarise(mean_gtb=mean(gtb))
 
+  #make the flower plot
   PlotFlower(
     petalDf$mean_gtb, rep(1, nrow(petalDf)), petalDf$Metric, center=round(mean(petalDf$mean_gtb),2),
     main = "Good to bad ratio", fill.col = RColorBrewer::brewer.pal(nrow(petalDf), 'Spectral'),
--- R/plotWeek.R
+++ R/plotWeek.R
@@ -13,10 +13,16 @@
 
 plotWeek <- function(sq_summary){
 
+  #first subset the dataframe to contain only the metric variable (x1) and the net scores for each week
   netCols <- dplyr::select(sq_summary, X1, net_w1, net_w2, net_w3, net_w4)
+
+  #then gather to get week as one variable
   netColsSum <- tidyr::gather(netCols, "week", "netScore", 2:5)
+
+  #rename the metric column
   names(netColsSum)[1]<-"Metric"
 
+  #create line plot of each metric over the 4 weeks
   s <- ggplot2::ggplot(netColsSum, aes(x= week, y= netScore, group=metric, colour=metric)) +
     geom_line() +
     geom_point() +
@@ -27,6 +33,7 @@ plotWeek <- function(sq_summary){
          y = "Net score") +
     scale_x_discrete(labels=c("Week 1", "Week 2", "Week 3", "Week 4"))
 
+  #wrap plot in plotly for interactive flashyness
   ggplotly(s, tooltip = "netScore")
 
 
